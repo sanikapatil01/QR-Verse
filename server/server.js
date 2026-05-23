6d69ebd383e963df8ff71a7c3711682e3381d12f
sanikapatil01 <sanikapatil01112006@gmail.com>
2026-05-23 11:27:01 +0530

    fixed backend connetion

--- server/server.js
+++ server/server.js
@@ -39,9 +39,9 @@ app.get("/", (req, res) => {
     res.send("QRVerse backend running. Use the frontend on the client dev server or build + serve the static site.");
 });
 
-const mongoUri = process.env.MONGO_URI || process.env.MONGO_URL;
+const mongoUri = process.env.MONGO_URI || process.env.MONGO_URI;
 
-mongoose.connect(mongoUri)
+mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("mongodb connected"))
 .catch((err)=>console.log(err));
 
